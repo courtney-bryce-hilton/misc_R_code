46df4aa73626341f9788fbf90c411887d5193586
courtney-bryce-hilton <courtney.bryce.hilton@gmail.com>
2023-05-08 10:45:53 +1200

    tweaked bib_extractor to account for "in press" cites

--- bib_extractor.R
+++ bib_extractor.R
@@ -10,26 +10,35 @@ p_load(
   bib2df
 )
 
+
+# parameters --------------------------------------------------------------
+
+
+manuscript_file_path <- here("writing", "manuscript.Rmd")
+bib_file_path <- here("writing", "themusiclab_long.bib")
+output_file_path <- here("writing", "themusiclab.bib")
+
+
 # extract cite keys from manuscript ---------------------------------------
 
 # load manuscript
-Rmd <- readChar(here("writing", "manuscript.rmd"),nchars=1e7)
+Rmd <- readChar(manuscript_file_path, nchars=1e9)
 
 # extract out all citekeys 
 cites <- str_extract_all(Rmd, "@[a-zA-Z0-9-]*(?=(\\s)|(;)|(])|(\\.)|($))")[[1]] %>% 
   unique() %>% 
   tibble() %>% 
-  filter(str_detect(., "\\d")) %>% 
+  filter(str_detect(., "\\d|(inpress)")) %>% 
   mutate(cite = str_remove(., "@")) %>% 
   pull(cite)
 
 # filter .bib file to just those keys -------------------------------------
 
-bib_out <- bib2df(here("writing", "themusiclab.bib")) %>% 
-  filter(BIBTEXKEY %in% all_of(cites)) %>% 
+bib_out <- bib2df(bib_file_path) %>% 
+  filter(BIBTEXKEY %in% cites) %>% 
   select(!contains("."))
 
-df2bib(bib_out, file = here("writing", "themusiclab_test.bib"))
+df2bib(bib_out, file = output_file_path)
 
 
 
